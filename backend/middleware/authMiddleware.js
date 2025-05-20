03b4f620557b7502ea09849363bef441d6cd2e84
Andreas Tremml <Andreas.Tremml@at-medical.de>
2025-05-20 14:24:40 +0200

    Update authMiddleware.js
    Signed-off-by: Andreas Tremml <Andreas.Tremml@at-medical.de>

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,15 +1,8 @@
-const jwt = require('jsonwebtoken');
-const config = require('../../config/config');
-
-module.exports = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (!token) return res.status(401).json({ error: 'Kein Token vorhanden' });
-
-  try {
-    const decoded = jwt.verify(token, config.jwtSecret);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    res.status(401).json({ error: 'Ungültiger Token' });
-  }
+exports.verifyToken = (req, res, next) => {
+    // Dummy-Prüfung (später JWT-Auth)
+    if (req.headers.authorization) {
+        next();
+    } else {
+        res.status(401).json({ message: 'Nicht autorisiert' });
+    }
 };
